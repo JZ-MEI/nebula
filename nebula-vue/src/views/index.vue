fc5ebdbd81b9f6d749ef93916a995c4bd54f5fa1
meijinzhi <1749092177@qq.com>
2024-06-10 23:15:04 +0800

    首页修改

--- nebula-vue/src/views/index.vue
+++ nebula-vue/src/views/index.vue
@@ -1,36 +1,155 @@
 <template>
     <div class="view-container">
-        test
+        <lay-row :space="30">
+            <lay-col :md = 8>
+                <lay-panel shadow="hover">
+                    <div ref = "pieChart" style="width:100%;height:30vh"></div>
+                </lay-panel>
+            </lay-col>
+            <lay-col :md = "24">
+                <lay-panel shadow="hover">
+                    <div ref="lineChart" style="width:100%;height:30vh"></div>
+                </lay-panel>
+            </lay-col>
+        </lay-row>
+
     </div>
 </template>
 
 <script>
-import { ref } from 'vue'
-import {useRouter} from "vue-router";
-import Layout from "@/layout/index.vue";
-import router from "@/router/index.js";
+import * as echarts from 'echarts';
+import {onMounted} from 'vue';
 
 export default {
     setup() {
-
-        const openKeys2 = ref(["7"])
-        const selectedKey = ref("5")
-        const changeSelectedKey = (val) => {
-            selectedKey.value = val;
+        const pieChart = ref();
+        const lineChart = ref();
+        function initPieChart(){
+            const pie = echarts.init(pieChart.value);
+            const option = {
+                grid:{
+                    top:'10%',
+                    bottom:'10%'
+                },
+                tooltip: {
+                    trigger: 'item'
+                },
+                legend: {
+                    top: '5%',
+                    left: 'center'
+                },
+                series: [
+                    {
+                        name: 'Access From',
+                        type: 'pie',
+                        radius: ['40%', '70%'],
+                        avoidLabelOverlap: false,
+                        itemStyle: {
+                            borderRadius: 10,
+                            borderColor: '#fff',
+                            borderWidth: 2
+                        },
+                        label: {
+                            show: false,
+                            position: 'center'
+                        },
+                        emphasis: {
+                            label: {
+                                show: true,
+                                fontSize: 40,
+                                fontWeight: 'bold'
+                            }
+                        },
+                        labelLine: {
+                            show: false
+                        },
+                        data: [
+                            { value: 1048, name: 'Search Engine' },
+                            { value: 735, name: 'Direct' },
+                            { value: 580, name: 'Email' },
+                            { value: 484, name: 'Union Ads' },
+                            { value: 300, name: 'Video Ads' }
+                        ]
+                    }
+                ]
+            };
+            pie.setOption(option)
+        }
+        function initLineChart(){
+            const line = echarts.init(lineChart.value);
+            const option = {
+                title: {
+                    text: 'Stacked Line'
+                },
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {
+                    data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                toolbox: {
+                    feature: {
+                        saveAsImage: {}
+                    }
+                },
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [
+                    {
+                        name: 'Email',
+                        type: 'line',
+                        stack: 'Total',
+                        data: [120, 132, 101, 134, 90, 230, 210]
+                    },
+                    {
+                        name: 'Union Ads',
+                        type: 'line',
+                        stack: 'Total',
+                        data: [220, 182, 191, 234, 290, 330, 310]
+                    },
+                    {
+                        name: 'Video Ads',
+                        type: 'line',
+                        stack: 'Total',
+                        data: [150, 232, 201, 154, 190, 330, 410]
+                    },
+                    {
+                        name: 'Direct',
+                        type: 'line',
+                        stack: 'Total',
+                        data: [320, 332, 301, 334, 390, 330, 320]
+                    },
+                    {
+                        name: 'Search Engine',
+                        type: 'line',
+                        stack: 'Total',
+                        data: [820, 932, 901, 934, 1290, 1330, 1320]
+                    }
+                ]
+            };
+            line.setOption(option)
         }
 
-        const changeOpenKeys = (val) => {
-            openKeys2.value = val;
-        }
-        const router = useRouter()
 
-
-
+        onMounted(()=>{
+            initLineChart()
+            initPieChart()
+        })
         return {
-            openKeys2,
-            selectedKey,
-            changeOpenKeys,
-            changeSelectedKey
+            pieChart,
+            lineChart
         }
     }
 }
